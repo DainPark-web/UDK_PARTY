f0b0955e2d63b176987be70a9f5f0eadcdb881d4
dain <57316771+DainPark-web@users.noreply.github.com>
2022-10-19 16:47:08 +0200

    update

--- js/iso/index.js
+++ js/iso/index.js
@@ -13,6 +13,23 @@ const cubes = [];
 let mic, fft;
 
 let time = 0;
+
+
+// making DB (LocalStorage)
+let peopleSound = []
+setInterval(() => {
+    if(localStorage.getItem("peopleSound")){
+        peopleSound = JSON.parse(localStorage.getItem("peopleSound"))
+    }
+    const soundConfig ={
+        soundLeve : volSize,
+        time: Date.now()
+    }
+    peopleSound.push(soundConfig)
+    localStorage.setItem("peopleSound", JSON.stringify(peopleSound))
+    
+
+}, 5000)
 function setup() {
     let cnv = createCanvas(WIDTH,HEIGHT)
     cnv.parent(canvasContainer)
@@ -23,13 +40,11 @@ function setup() {
 
   cubes.push(new Cube(0, 0, 0));
 
-//   while (cubes.length < 10) {
-//     addRandomCube();
-//   }
+
     const cCount = 10;
     const cW = 10
     for(let i = 0; i < cCount; i++){
-        // cubes.push(new Cube((i * 2), i  , i ))
+      
         for(let j = 0; j< cCount; j++){
             cubes.push(new Cube(i,j,4))
 
@@ -59,30 +74,17 @@ function draw() {
   volSize = map(vol, 0, 1, 0, 10000)
   volSizeE = map(vol, 0, 1, 0, 100)
   var spectrum = fft.analyze();
-// time += 0.1;
+
   for(let i = 0; i< cubes.length; i++){
 
       cubes[i].draw();
-    //   cubes[i].update(sin(time) * i * 0.5, sin(time) * i)
   }
 
-//   for(let i = 0; i < spectrum.length; i++){
-//     let x = spectrum.length - i;
-//     let y = spectrum[i];
-//     // colorMode(HSB, 100);
-//     // fill(y , y, x)
 
-//     cubes[Math.floor(random(i))].update(-(volSize * (x * 0.01)),-(volSize * (x * 0.01)))
-    
-// }
 let y =0;
   for(let i = 0; i < cubes.length; i++){
-    // let x = spectrum.length - i;
-    // let y = spectrum[i];
-    // colorMode(HSB, 100);
-    // fill(y , y, x)
+   
     const cubeC = Math.floor(random(cubes.length))
-    const x = cubes.length - i
     if(i < spectrum.length){
 
         y = spectrum[i];
@@ -96,5 +98,24 @@ let y =0;
     }
     
 }
+
+    //error Handling
+    push()
+    const rectW = 180;
+    fill(46,49,145)
+    rect(WIDTH - rectW - 20, 20, rectW, HEIGHT- 40, 20)
+    if(localStorage.getItem("peopleSound")){
+        const getPartySound = JSON.parse(localStorage.getItem("peopleSound"))
+    for(let i = 0; i < getPartySound.length; i++){
+        fill(255)
+        // text(Math.floor(getPartySound[i].soundLeve), (WIDTH - rectW) + 80, (i * 20) + 40);
+        const rectWW = map(Math.floor(getPartySound[i].soundLeve), 0, 5000, 2, 100)
+        noStroke()
+        rect( ((WIDTH - rectW) + 90) - rectWW, (i * 20) + 40 - 5, rectWW,5);
+        text((new Date(getPartySound[i].time)).toString().slice(16,24), (WIDTH - rectW) + 100, (i * 20) + 40);
+        // rect(WIDTH - 200, i * 10, 500,10);
+    }
+    }
+    pop()
 }
 
